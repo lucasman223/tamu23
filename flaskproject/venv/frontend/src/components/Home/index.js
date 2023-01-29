388dcf00a2eff67f6e730c347ddb3a887bf7bf67
Andy Wang <WA287142@tamu.edu>
2023-01-29 11:27:32 -0600

    connected backfront

--- flaskproject/venv/frontend/src/components/Home/index.js
+++ flaskproject/venv/frontend/src/components/Home/index.js
@@ -2,20 +2,55 @@ import './index.scss';
 import Sidebar from '../Sidebar';
 import { Outlet } from 'react-router-dom'
 import Layout from '../Layout';
-
+import { useNavigate } from "react-router-dom";
+import { useState } from 'react';
+import axios from 'axios';
 
 import { useSpring, animated } from '@react-spring/web'
 
-const PythonShell = require('python-shell');
-const options = {
-  scriptPath: 'path/to/python/script'
-};
+// useEffect(()=>{
+//       axios.post('http://localhost:5000', {
+//       fnum: '123',
+//       date: '13/22/22'
+//       }).then(response => {
+//         console.log("SUCCESS", response)
+//         setGetMessage(response)
+//       }).catch(error => {
+//         console.log(error)
+//       })
+
+//     }, [])
 const Home = () => {
+  const [flightNum, setFlightNum] = useState("");
+  const [date, setDate] = useState("");
+  const [backendData, setBackendData] = useState({});
+
+
     const springs = useSpring({
         from: { x: -3000 },
         to: { x: 100 },
       })
 
+let navigate = useNavigate();
+const routeChange = () =>{ 
+  let path = `/Result`; 
+  navigate(path);
+}
+
+  function submit()
+  {
+    axios.post('http://localhost:5000', {
+      flightNum: flightNum,
+      date: date
+    })
+    .then(function (response) {
+      let bd = response.data;
+      console.log(bd)
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
 
     return (
             
@@ -30,16 +65,23 @@ const Home = () => {
               borderRadius: 8,
               ...springs,
             }}>
-              <h1>Product Name, Short Description of Program
-                </h1></animated.div>
+              <h1>FlightForecast </h1> 
+              <h2>Determining the Likelihood of Flight Delays Due to Weather Conditions 
+                {/* FIND HOW TO INCREASE WIDTH SO ITS NOT AS VERTICAL */}
+                </h2></animated.div>
                 </div>
                 <div className="form-group">
-                <form>
+                <div>
                   <label>Enter Your Flight Number <br />
-                    <input name="flightnum" placeholder='Ex. DAL748'/>
+                    <input name="flightnum" placeholder='Ex. 748' value={flightNum} onChange={(event) => setFlightNum(event.target.value)}/>
+                    <p>{flightNum}</p>
                   </label>
-                  <button type='submit' onClick={request.get('http://google.com/')}>Submit</button>
-                </form>
+                  <label>Enter the Date of Flight <br />
+                    <input name="date" placeholder='Ex. 1/29/2023' value={date} onChange={(event) => setDate(event.target.value)}/>
+                    <p>{date}</p>
+                  </label>
+                  <button type='submit' onClick={submit}>Submit</button>
+                </div>
           
             </div>
         </div>
