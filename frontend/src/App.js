fc2dc14cf256ce911e898d49ef19f40987536262
lucasman223 <lucasman223@gmail.com>
2023-01-28 18:18:25 -0600

    About page poc

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import './App.scss';
 import Layout from './components/Layout';
+import About from "./components/About";
 import { Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <>
+    <Layout/>
     <Routes>
-      <Route path="/" element={<Layout/>} />
+      
+      <Route path="/about" element={<About/>} />
     </Routes>
     </>
   );
