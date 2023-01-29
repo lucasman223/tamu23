3b94470f8afc8a2c4dbb62e93cfd7797ce09b3f5
Andy Wang <WA287142@tamu.edu>
2023-01-29 00:38:44 -0600

    Added Flask API

---
+++ flaskproject/venv/frontend/src/App.js
@@ -0,0 +1,57 @@
+// import Layout from './components/Layout';
+// import About from "./components/About";
+// import { Routes, Route} from 'react-router-dom'
+// import Home from './components/Home'
+// import Result from './components/Result'
+
+// function App() {
+//   return (
+//     <>
+//     <Layout/>
+//     <Routes>
+      
+//       <Route path="/about" element={<About/>} />
+//       <Route path="/" element={<Layout/>} />
+//       <Route path="/Result" element={<Result/>} />
+//       <Route index element={<Home/>} />
+//     </Routes>
+//     </>
+//   );
+// }
+
+// export default App;
+
+
+
+//import logo from './logo.svg';
+//import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+function App() {
+  const [getMessage, setGetMessage] = useState({})
+
+  useEffect(()=>{
+    axios.get('http://localhost:5000/flask/hello').then(response => {
+      console.log("SUCCESS", response)
+      setGetMessage(response)
+    }).catch(error => {
+      console.log(error)
+    })
+
+  }, [])
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>React + Flask Tutorial</p>
+        <div>{getMessage.status === 200 ? 
+          <h3>{getMessage.data.message}</h3>
+          :
+          <h3>LOADING</h3>}</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ flaskproject/venv/frontend/src/components/About/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+
+export default function About() {
+    return (
+        <div className='container about-page'>
+            <h1>About Page</h1>
+            <div>
+                this is random text
+            </div>
+        </div>
+    )
+}
